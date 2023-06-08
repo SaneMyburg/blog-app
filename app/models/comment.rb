5549567bb8be4c706086de03aa9c9c9afe98c551
SaneMyburg <sane.myburg@gmail.com>
2023-06-08 03:32:35 +0200

    Add method to update post comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_create :update_post_comments_counter
+  after_destroy :update_post_comments_counter
+
+  private
+
+  def update_post_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
+
 end
