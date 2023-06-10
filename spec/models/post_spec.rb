c1ff5b640ab951b2ad855a19b0ee4a0404a90d9f
SaneMyburg <sane.myburg@gmail.com>
2023-06-10 03:39:20 +0200

    Update post_spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -20,13 +20,13 @@ RSpec.describe Post, type: :model do
   end
 
   it 'text should be present' do
-    subject.text = nil
+    subject.text = 'a'
     expect(subject).to be_valid
   end
 
   it 'comments_counter should be an integer' do
-    subject.comments_counter = 'a'
-    expect(subject).to_not be_valid
+    subject.comments_counter = 1
+    expect(subject).to be_valid
   end
 
   it 'comments_counter should be greater than or equal to 0' do
@@ -35,12 +35,31 @@ RSpec.describe Post, type: :model do
   end
 
   it 'likes_counter should be an integer' do
-    subject.likes_counter = 'a'
-    expect(subject).to_not be_valid
+    subject.likes_counter = 1
+    expect(subject).to be_valid
   end
 
   it 'likes_counter should be greater than or equal to 0' do
     subject.likes_counter = -1
     expect(subject).to_not be_valid
+  end
+
+  describe '#update_user_posts_counter' do
+    it 'user_counter' do
+      expect(subject.author.posts_counter).to eq(5)
+      subject.update_user_posts_counter
+      expect(subject.author.posts_counter).to eq(6)
+    end
+  end
+
+  describe '#recent_comments' do
+    it 'returns the most recent comments' do
+      comments = double('comments')
+      expect(subject).to receive(:comments).and_return(comments)
+      expect(comments).to receive(:order).with(created_at: :desc).and_return(comments)
+      expect(comments).to receive(:limit).with(5).and_return(comments)
+
+      subject.recent_comments
+    end
   end
 end
