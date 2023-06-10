cbf3230fda53bda58fa75de3e01256677bd926d8
SaneMyburg <sane.myburg@gmail.com>
2023-06-10 05:51:34 +0200

    Update user_spec.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments, class_name: 'Comment', foreign_key: :post_id
   has_many :likes, class_name: 'Like', foreign_key: :post_id
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,4 +23,28 @@ RSpec.describe User, type: :model do
     subject.posts_counter = -1
     expect(subject).to_not be_valid
   end
+
+  describe '.recent_posts' do
+    let(:user) { User.create(name: 'John') }
+
+    it 'returns the most recent 3 posts of the user' do
+      recent_posts = [
+        Post.create(title: 'First Post', text: 'Hello', comments_counter: 0, likes_counter: 0, author: user, created_at: 3.days.ago),
+        Post.create(title: 'Second Post', text: 'World', comments_counter: 0, likes_counter: 0, author: user, created_at: 2.days.ago),
+        Post.create(title: 'Third Post', text: 'Test', comments_counter: 0, likes_counter: 0, author: user, created_at: 1.day.ago)
+      ]
+    
+      recent_posts.each { |post| post.update(author_id: user.id) }
+    
+      expected = recent_posts.map { |post| post.attributes.slice('id', 'title', 'text', 'comments_counter', 'likes_counter', 'author_id', 'created_at', 'updated_at') }
+      actual = User.recent_posts(user).map { |post| post.attributes.slice('id', 'title', 'text', 'comments_counter', 'likes_counter', 'author_id', 'created_at', 'updated_at') }
+    
+      expected.sort_by! { |post| post['created_at'] }
+      actual.sort_by! { |post| post['created_at'] }
+    
+      expect(expected).to eq(actual)
+      expect(actual.all? { |post| post['author_id'] == user.id }).to be_truthy
+    end
+      
+  end
 end
