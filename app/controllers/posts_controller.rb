d1d90307a4e4620cc4997dfd9500f45ba78e5c5a
SaneMyburg <sane.myburg@gmail.com>
2023-06-12 16:12:29 +0200

    Add action in dedicated controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.recent_comments
+  end
+end
