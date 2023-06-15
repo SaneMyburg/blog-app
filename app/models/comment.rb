043da63c87a2045b7d5587ab25f3bce5d1aa5c02
SaneMyburg <sane.myburg@gmail.com>
2023-06-16 01:43:33 +0200

    Fix bug with comments counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post, class_name: 'Post', foreign_key: :post_id
 
-  # validates :text, presence: true
+  validates :text, presence: true
 
   after_create :update_post_comments_counter
   after_destroy :update_post_comments_counter
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,10 @@ class Post < ActiveRecord::Base
   has_many :comments, class_name: 'Comment', foreign_key: :post_id
   has_many :likes, class_name: 'Like', foreign_key: :post_id
 
-  # validates :title, presence: true, length: { maximum: 250 }
-  # validates :text, presence: true
-  # validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  # validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :text, presence: true
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_create :update_user_posts_counter
 
