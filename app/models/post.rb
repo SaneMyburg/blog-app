e0b55b29903568b6ca0d8a0607dc620656734672
SaneMyburg <sane.myburg@gmail.com>
2023-06-16 00:54:30 +0200

    Refactor to show display user details

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,19 @@ class Post < ActiveRecord::Base
   has_many :comments, class_name: 'Comment', foreign_key: :post_id
   has_many :likes, class_name: 'Like', foreign_key: :post_id
 
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :text, presence: true
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :title, presence: true, length: { maximum: 250 }
+  # validates :text, presence: true
+  # validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_create :update_user_posts_counter
 
   def update_user_posts_counter
     author.update(posts_counter: author.posts.count)
+  end
+
+  def comments_counter
+    comments.count
   end
 
   def recent_comments(limit = 5)
