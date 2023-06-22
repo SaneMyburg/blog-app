a8f83782a7fc4c71b56682449003ca11f3b520d4
SaneMyburg <sane.myburg@gmail.com>
2023-06-22 10:14:17 +0200

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,39 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = current_user
     @posts = @user.posts.order(created_at: :asc)
+    @current_user = current_user
   end
 
   def show
     @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
     @comments = @post.recent_comments
+    @comment = Comment.new
+    @current_user = current_user
+  end
+
+  def new
+    puts "New action called"
+    @post = Post.new
+  end
+
+  def create
+    puts current_user.inspect
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    if @post.save
+      redirect_to user_post_path(current_user, @post), notice: 'Post created successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
