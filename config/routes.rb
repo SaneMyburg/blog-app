84d80fe9d00e8627ed886d76e5fa074282e489c6
SaneMyburg <sane.myburg@gmail.com>
2023-06-22 11:40:43 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
+      member do
+        post :like
+      end
     end
   end
 end
